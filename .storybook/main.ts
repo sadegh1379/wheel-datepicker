e5ef6bf44e43ceac46fb8bc0d598c13233a0eaad
Sadegh Akbari <akbarisadegh382@gmail.com>
2025-08-05 10:43:40 +0330

    feat: update Storybook configuration and remove deprecated datepicker test stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,14 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const config: StorybookConfig = {
-  'stories': ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  'stories': [
+    '../src/**/*.mdx',
+    '../src/**/stories/*.stories.@(js|jsx|mjs|ts|tsx)',
+    ...(!isProduction ? ['../src/**/__test__/*.stories.@(js|jsx|mjs|ts|tsx)'] : [])
+    
+  ],
   'addons': ['@storybook/addon-webpack5-compiler-swc', '@storybook/addon-docs'],
   'framework': {
     'name': '@storybook/react-webpack5',
